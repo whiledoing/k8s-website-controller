27cb10f80ad4807a3270d14e0e7c8eb373ddc4fe
jiangjinzheng <jiangjinzheng@bytedance.com>
2020-10-01 22:21:39 +0800

    feat: change config for new k8s group/version

--- pkg/website-controller.go
+++ pkg/website-controller.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"encoding/json"
+	"fmt"
 	"io"
-	"log"
-	"github.com/luksa/website-controller/pkg/v1"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"strings"
+
+	v1 "github.com/luksa/website-controller/pkg/v1"
 )
 
 func main() {
@@ -26,7 +27,7 @@ func main() {
 			if err := decoder.Decode(&event); err == io.EOF {
 				break
 			} else if err != nil {
-				log.Fatal(err)
+				log.Fatalf("meet decode error: event=%s: err=%s", event, err)
 			}
 
 			log.Printf("Received watch event: %s: %s: %s\n", event.Type, event.Object.Metadata.Name, event.Object.Spec.GitRepo)
@@ -43,12 +44,12 @@ func main() {
 
 func createWebsite(website v1.Website) {
 	createResource(website, "api/v1", "services", "service-template.json")
-	createResource(website, "apis/extensions/v1beta1", "deployments", "deployment-template.json")
+	createResource(website, "apis/apps/v1", "deployments", "deployment-template.json")
 }
 
 func deleteWebsite(website v1.Website) {
-	deleteResource(website, "api/v1", "services", getName(website));
-	deleteResource(website, "apis/extensions/v1beta1", "deployments", getName(website));
+	deleteResource(website, "api/v1", "services", getName(website))
+	deleteResource(website, "apis/apps/v1", "deployments", getName(website))
 }
 
 func createResource(webserver v1.Website, apiGroup string, kind string, filename string) {
@@ -60,11 +61,16 @@ func createResource(webserver v1.Website, apiGroup string, kind string, filename
 	template := strings.Replace(string(templateBytes), "[NAME]", getName(webserver), -1)
 	template = strings.Replace(template, "[GIT-REPO]", webserver.Spec.GitRepo, -1)
 
-	resp, err := http.Post(fmt.Sprintf("http://localhost:8001/%s/namespaces/%s/%s/", apiGroup, webserver.Metadata.Namespace, kind), "application/json", strings.NewReader(template))
+	url := fmt.Sprintf("http://localhost:8001/%s/namespaces/%s/%s/", apiGroup, webserver.Metadata.Namespace, kind)
+	log.Printf("url=%s: data=%s", url, template)
+	resp, err := http.Post(url, "application/json", strings.NewReader(template))
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("response Status:", resp.Status)
+
+	message, _ := ioutil.ReadAll(resp.Body)
+	log.Println("response Message:", string(message))
 }
 
 func deleteResource(webserver v1.Website, apiGroup string, kind string, name string) {
@@ -80,9 +86,8 @@ func deleteResource(webserver v1.Website, apiGroup string, kind string, name str
 		return
 	}
 	log.Println("response Status:", resp.Status)
-
 }
 
 func getName(website v1.Website) string {
-	return website.Metadata.Name + "-website";
+	return website.Metadata.Name + "-website"
 }
